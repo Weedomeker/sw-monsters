0c2ec5c8742549c96b958ee7520dfa46d202dfe4
Weedomeker <weedomeker@gmail.com>
2022-12-13 19:44:42 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  // States
+  let [number, setNumber] = useState(0)
+
+  // Comportements
+  const handleClickIncremente = () => {
+    setNumber(number + 1)
+  }
+  const handleClickDecremente = () => {
+    number <= 0 ? setNumber(0) : setNumber(number - 1)
+  }
+
+  // Affichage rendu
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h2>My number: {number}</h2>
+
+      <button type="button" onClick={handleClickIncremente}>
+        +1
+      </button>
+      <button type="button" onClick={handleClickDecremente}>
+        -1
+      </button>
+      {/* { number ? number < 1 : setNumber(number === 0) } */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
